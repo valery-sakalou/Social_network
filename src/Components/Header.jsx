59c4a6462bb971b499e01bf6df5d9d998a8126e2
Valeriy <mimino730@tut.by>
2021-12-13 23:38:06 +0300

    13st lesson

---
+++ src/Components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () =>{
+    return <header className="header">
+    <img src="https://w7.pngwing.com/pngs/41/701/png-transparent-logo-graphic-design-graphy-grapher-logo-graphy-angle-text-trademark.png" />
+  </header>;
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,24 @@
+const Profile = () =>{
+  return <div className="content">
+  <div>
+    <img src="https://n4.sdlcdn.com/imgs/i/g/n/galaxy-wooden-art-gallery-Natural-SDL910168366-1-7012a.jpg" />
+  </div>
+  <div>
+    ava+description<img src="https://i.pinimg.com/originals/65/41/1a/65411a51ae58ed21711131ac3918846b.jpg"/>
+  </div>
+  <div>
+    My posts
+    <div>
+      New post
+    </div>
+    <div>
+      Post1
+    </div>
+    <div>
+      Post2
+    </div>
+  </div>
+</div>
+}
+
+export default Profile;
